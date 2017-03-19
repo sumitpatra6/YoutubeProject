623f1e061d22c3679c434668083657c4294e4fcd
sumitpatra6@gmail.com <sumitpatra6@gmail.com>
2017-03-19 19:48:14 +0530

    file commit for queue function

---
+++ You/WebContent/assets/qu.js
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+//script file for youtube project
+//BBSE4dA4aLg,IZ0IhkGSXN0,BBSE4dA4aLg
+var videoBoxArray = ['DhDzYg0i1HY','IZ0IhkGSXN0','Xcm4ZBR-VCc'];
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag,firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady(){
+  console.log('API fired');
+  player = new YT.Player('player',{
+    height : '390',
+    width : '640',
+    videoId : videoBoxArray[0],
+    events :{
+      'onReady' : onPlayerReady,
+      'onStateChange' : 'onPlayerStateChange'
+    }
+  });
+}
+
+function onPlayerReady(event){
+  console.log('onReady function fired!');
+  //videoBoxArray.shift();
+  console.log(videoBoxArray);
+  event.target.playVideo();
+}
+var done = false;
+function onPlayerStateChange(event){
+  if(event.data == YT.PlayerState.ENDED ){
+    console.log("video ended!");
+    var id = videoBoxArray[0];
+    videoBoxArray.shift();
+    event.target.loadVideoById(id,5,"large");
+
+  }
+}
+
+function stopVideo(){
+  player.stopVideo();
+}
