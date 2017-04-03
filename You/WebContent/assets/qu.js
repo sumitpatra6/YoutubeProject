c0dfeb33b74dfd75dd3d980681889d6ad5be19e3
sumitpatra6@gmail.com <sumitpatra6@gmail.com>
2017-04-03 22:46:34 +0530

    first working commit

--- You/WebContent/assets/qu.js
+++ You/WebContent/assets/qu.js
@@ -8,43 +8,45 @@ var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag,firstScriptTag);
-
+var i;
 var player;
 function onYouTubeIframeAPIReady(){
-  console.log('API fired');
-  player = new YT.Player('player',{
-    height : '390',
-    width : '640',
-    videoId : videoBoxArray[0],
-    events :{
-      'onReady' : onPlayerReady,
-      'onStateChange' : 'onPlayerStateChange'
-    }
-  });
+	i =0;
+	console.log('API fired');
+	player = new YT.Player('player',{
+		height : '390',
+		width : '640',
+		videoId : videoBoxArray[0],
+		events :{
+			'onReady' : onPlayerReady,
+			'onStateChange' : 'onPlayerStateChange'
+		}
+	});
 }
 
 function onPlayerReady(event){
-  console.log('onReady function fired!');
-  //videoBoxArray.shift();
-  console.log(videoBoxArray);
-  event.target.playVideo();
+	console.log('onReady function fired!');
+	//videoBoxArray.shift();
+	console.log(videoBoxArray);
+	event.target.playVideo();
 }
 var done = false;
 function onPlayerStateChange(event){
-  if(event.data == YT.PlayerState.ENDED ){
-    console.log("video ended!");
-    cleanQueue(videoBoxArray[0]);
-    console.log(event);
-    
-    videoBoxArray.shift();
-    var id = videoBoxArray[0];
-    event.target.loadVideoById(id,5,"large");
+	if(event.data == YT.PlayerState.ENDED ){
+		i++;
+		console.log("video ended!");
+		//cleanQueue(videoBoxArray[0]);
+		console.log(event);
 
-  }
+		//videoBoxArray.shift();
+		var id = videoBoxArray[i];
+		event.target.loadVideoById(id,5,"large");
+
+	}
 }
 
 function stopVideo(){
-  player.stopVideo();
+	player.stopVideo();
 }
 
 function playThis(id){
@@ -57,7 +59,7 @@ function playThis(id){
 </div> */
 function enqueue(id,title,url){
 	console.log(id+" "+title+" "+url);
-	var playListString = "<div class='playlist-item' id ='"+id+"' onclick='playThis('"+id+"')'><img src='"+url+"' alt='Thumb Not Found'/></div>"
+	var playListString = "<div class='playlist-item' id ='"+id+"' onclick=playThis('"+id+"')><img src='"+url+"' alt='Thumb Not Found'/></div>"
 	$("#playList").append(playListString);
 	postEnqueueAction(id);
 }
@@ -68,9 +70,13 @@ function postEnqueueAction(id){
 }
 
 function playThis(id){
-	console.log('id');
+	console.log(id);
+	i = videoBoxArray.indexOf(id);
+	player.loadVideoById(id,5,'large');
 }
 
+
+//not required
 function cleanQueue(id){
 	//TODO: clean the queue
 }
